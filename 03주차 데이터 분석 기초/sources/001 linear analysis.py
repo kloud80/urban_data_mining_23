b68056ea55c72ddceab9d90370bda9e12759192d
Bigvalue <kloud80@gmail.com>
2023-09-17 15:43:45 +0900

    2주차 강의 자료

---
+++ 03주차 데이터 분석 기초/sources/001 linear analysis.py
@@ -0,0 +1,267 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""===============================================================================
+국토교통부 실거래가 불러오기
+http://rtdown.molit.go.kr/
+아파트 2023년 8월 1달간 전국 실거래 정보
+==============================================================================="""
+data = pd.read_csv('03주차 데이터 분석 기초/data/아파트(매매)__실거래가_20230916101223.csv', sep=',', skiprows=15, encoding='cp949', dtype='str')
+
+data.dtypes
+data.columns
+data.shape
+
+
+data['거래금액(만원)'] = data['거래금액(만원)'].str.replace(',', '')
+data['거래금액(만원)'] = data['거래금액(만원)'].astype('int')
+
+data = data.rename(columns={'거래금액(만원)' : '거래가'})
+
+data['거래가'].describe()
+data['거래가'].hist(bins=100)
+plt.show()
+
+tmp = data['시군구'].value_counts()
+tmp = tmp.reset_index()
+
+data = data[data['거래가'] < 50000]
+
+
+
+"""===============================================================================
+데이터 모델을 통한 해석
+==============================================================================="""
+
+data['거래가'].plot()
+plt.show()
+
+total_error = {}
+
+#평균
+model = data['거래가'].mean()
+error = data['거래가'] - model
+error = error * error
+total_error['mean'] = error.sum()
+
+#중앙
+model = data['거래가'].median()
+error = data['거래가'] - model
+error = error * error
+total_error['median'] = error.sum()
+
+#최빈
+model = data.mode(numeric_only=True, axis=0).values[0,0]
+error = data['거래가'] - model
+error = error * error
+total_error['mode'] = error.sum()
+
+
+model = data['거래가'].mean()
+model = np.full([data.shape[0],1], model)
+plt.plot(data['거래가'].values[:20])
+plt.plot(model[:20, 0])
+plt.show()
+
+
+"""===============================================================================
+데이터 추가
+==============================================================================="""
+data['전용면적(㎡)']
+data = data.rename(columns={'전용면적(㎡)':'면적'})
+data['면적'] = data['면적'].astype('float')
+
+data.dtypes
+
+
+from sklearn.linear_model import LinearRegression
+
+
+lm = LinearRegression()
+
+X = np.array(data['면적'].values)
+Y = np.array(data['거래가'].values)
+
+X = X[:, np.newaxis]
+Y = Y[:, np.newaxis]
+
+
+lm.fit(X, Y)
+
+lm.intercept_
+lm.singular_
+
+Y_hat = lm.predict(X)
+
+plt.plot(Y[:50])
+plt.plot(Y_hat[:50])
+plt.show()
+
+
+
+"""===============================================================================
+잔차 정규성
+==============================================================================="""
+
+plt.hist(Y, bins=100)
+plt.show()
+
+
+residuals = Y - Y_hat
+plt.hist(residuals, bins=100)
+plt.show()
+
+
+"""===============================================================================
+모델 설명력
+==============================================================================="""
+plt.title('TSS')
+plt.plot(Y[:50])
+plt.plot(model[:50])
+plt.fill_between(range(50), Y[:50,0], model[:50,0], color='red', alpha=0.4)
+plt.show()
+
+
+plt.title('SSR')
+plt.plot(Y[:50])
+plt.plot(Y_hat[:50])
+plt.fill_between(range(50), Y[:50,0], Y_hat[:50, 0], color='blue', alpha=0.4)
+plt.show()
+
+
+plt.title('SST vs SSR')
+plt.plot(Y[:50])
+plt.fill_between(range(50), Y[:50,0], model[:50,0], color='red', alpha=0.4)
+plt.fill_between(range(50), Y[:50,0], Y_hat[:50, 0], color='blue', alpha=0.4)
+plt.show()
+
+
+SST = np.sum(np.power(Y - np.mean(Y), 2))
+SSR = np.sum(np.power(Y - Y_hat, 2))
+
+1 - SSR/SST
+
+
+from sklearn.metrics import r2_score
+r2_score(Y, Y_hat)
+
+
+"""===============================================================================
+모델 설명력
+==============================================================================="""
+data['건축년도'] = data['건축년도'].astype('float')
+data['건축년도'] = data['건축년도'].fillna(1950)
+
+lm = LinearRegression()
+
+X = np.array(data[['면적','건축년도']].values)
+Y = np.array(data['거래가'].values)
+
+X = X[:]
+Y = Y[:, np.newaxis]
+
+
+lm.fit(X, Y)
+
+lm.intercept_
+lm.singular_
+
+Y_hat = lm.predict(X)
+
+plt.plot(Y[:50])
+plt.plot(Y_hat[:50])
+plt.show()
+
+
+r2_score(Y, Y_hat)
+
+
+
+"""===============================================================================
+모델 설명력
+==============================================================================="""
+data['층'] = data['층'].astype('int')
+data['시도'] = data['시군구'].apply(lambda x : x.split(' ')[0])
+
+data_dummies = pd.get_dummies(data['시도'] )
+data_dummies.columns
+data = pd.concat([data, data_dummies], axis=1)
+
+data['년식'] = 2023 - data['건축년도']
+data['년식제곱'] = data['년식']  * data['년식']
+
+
+lm = LinearRegression()
+
+X = np.array(data[['면적', '년식', '년식제곱', '강원특별자치도', '경기도', '경상남도', '경상북도', '광주광역시', '대구광역시', '대전광역시', '부산광역시',
+       '서울특별시', '세종특별자치시', '울산광역시', '인천광역시', '전라남도', '전라북도', '제주특별자치도', '충청남도',
+       '충청북도']].values)
+Y = np.array(data['거래가'].values)
+
+X = X[:]
+Y = Y[:, np.newaxis]
+
+
+lm.fit(X, Y)
+
+lm.intercept_
+lm.singular_
+
+Y_hat = lm.predict(X)
+
+plt.title('r2 : ' + str(r2_score(Y, Y_hat)))
+plt.plot(Y[:50])
+plt.plot(Y_hat[:50])
+plt.show()
+
+
+
+
+
+"""===============================================================================
+변수 설명력
+==============================================================================="""
+import statsmodels.api as sm
+results = sm.OLS(Y, sm.add_constant(X)).fit()
+print(results.summary())
+
+
+"""===============================================================================
+변수 설명력
+==============================================================================="""
+
+data['년식2'] = data['년식'] * (100 + 10 * np.random.random([data.shape[0]])) / 100
+data['년식3'] = data['년식'] + 3 * np.random.random([data.shape[0]])
+data['년식4'] = data['년식'] + 4 * np.random.random([data.shape[0]])
+
+lm = LinearRegression()
+
+X = np.array(data[['면적', '년식', '년식2', '년식3', '년식4', '년식제곱', '강원특별자치도', '경기도', '경상남도', '경상북도', '광주광역시', '대구광역시', '대전광역시', '부산광역시',
+       '서울특별시', '세종특별자치시', '울산광역시', '인천광역시', '전라남도', '전라북도', '제주특별자치도', '충청남도',
+       '충청북도']].values)
+Y = np.array(data['거래가'].values)
+
+X = X[:]
+Y = Y[:, np.newaxis]
+
+
+lm.fit(X, Y)
+
+lm.intercept_
+lm.singular_
+
+Y_hat = lm.predict(X)
+
+plt.title('r2 : ' + str(r2_score(Y, Y_hat)))
+plt.plot(Y[:50])
+plt.plot(Y_hat[:50])
+plt.show()
+
+results = sm.OLS(Y, sm.add_constant(X)).fit()
+print(results.summary())
+r2_score(Y, Y_hat)
+
+0.6383473412012983
+0.6383500553317636
+0.6383724079620556
---
+++ 03주차 데이터 분석 기초/sources/PCA.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+
+#임의의 분포를 만든다
+X = np.random.normal(0,1,1000)
+
+Y = X * 1 + np.random.normal(0,1,1000)
+
+Y = np.random.normal(0,1,1000)
+
+Y = X * -1 + np.random.normal(0, 1,1000)
+
+
+
+#분포 출력
+plt.scatter(X, Y)
+plt.show()
+
+
+
+
+#개별 분산 및 공분산을 계산한다.
+공분산 = np.cov(X, Y)
+print(공분산)
+
+
+
+#공분산 행렬의 의미
+균등좌표 = (np.arange(41) -20) * 0.1
+균등좌표 = np.full([41,41], 균등좌표)
+균등좌표 = np.concatenate([균등좌표[:,:, np.newaxis], 균등좌표.T[:,:, np.newaxis]], axis=2)
+
+
+#균등좌표계 출력
+plt.scatter(균등좌표[:,:,0], 균등좌표[:,:,1],s=0.5)
+plt.show()
+
+
+
+#공분산 행렬로 변환
+변환좌표 = 균등좌표 @ 공분산
+
+
+
+#변환된 좌표 출력
+plt.scatter(변환좌표[:,:,0], 변환좌표[:,:,1],s=0.5)
+plt.show()
+
+
+
+#고유벡터
+각도 = np.arange(360)
+각도 = 각도 * np.pi / 180
+각도_X = np.cos(각도)
+각도_Y = np.sin(각도)
+원래각도 = np.concatenate([각도_X[:,np.newaxis], 각도_Y[:,np.newaxis]], axis=1)
+
+plt.scatter(원래각도[:,0], 원래각도[:,1],s=0.5)
+plt.show()
+
+
+
+#공분산행렬로 변환한다.
+행렬변환 = 원래각도 @ 공분산
+
+plt.scatter(행렬변환[:,0], 행렬변환[:,1],s=0.5)
+plt.show()
+
+
+
+def dist(v):
+    return math.sqrt(v[0]**2 + v[1]**2)
+
+def getDegree(v):
+    v1 = v[:2]
+    v2 = v[2:]
+    # 벡터 v1, v2의 크기 구하기
+    distA = dist(v1)
+    distB = dist(v2)
+
+    # 내적 1 (x1x2 + y1y2)
+    ip = v1[0] * v2[0] + v1[1] * v2[1]
+
+    # 내적 2 (|v1|*|v2|*cos x)
+    ip2 = distA * distB
+
+    # cos x값 구하기
+    cost = ip / ip2
+
+    # x값(라디안) 구하기 (cos 역함수)
+    x = math.acos(cost)
+
+    # x값을 x도로 변환하기
+    degX = math.degrees(x)
+    return degX
+
+
+기준벡터 = np.full([360,2], np.array([1,0]))
+기준벡터 = np.concatenate([기준벡터, 행렬변환], axis=1)
+
+변환각도 = np.apply_along_axis(getDegree, 1, 기준벡터) / 180 * np.pi
+
+변환각도 = np.cos(변환각도)
+
+각도차이 = 각도_X - 변환각도
+각도차이 = 각도차이 * 각도차이
+
+np.where(각도차이 < 0.001)
+
+
+plt.scatter(원래각도[:,0], 원래각도[:,1],s=0.5)
+plt.scatter(원래각도[np.where(각도차이 < 0.00001),0], 원래각도[np.where(각도차이 < 0.00001),1],s=5, marker='X', color='#FF0000')
+plt.show()
+
+
+plt.scatter(행렬변환[:,0], 행렬변환[:,1],s=0.5)
+plt.scatter(행렬변환[np.where(각도차이 < 0.00001),0], 행렬변환[np.where(각도차이 < 0.00001),1],s=5, marker='X', color='#FF0000')
+plt.show()
+
+
+plt.scatter(X, Y, alpha=0.2)
+plt.scatter(행렬변환[:,0], 행렬변환[:,1],s=0.5)
+plt.scatter(행렬변환[np.where(각도차이 < 0.00001),0], 행렬변환[np.where(각도차이 < 0.00001),1],s=20, marker='X', color='#FF0000')
+plt.show()
+
+
+
+"""===============================================================================
+주성분 분석
+==============================================================================="""
+
+from sklearn.decomposition import PCA
+data = np.concatenate([X[:,np.newaxis],Y[:,np.newaxis]], axis=1)
+
+pca = PCA(n_components = 2) # feature 변수 개수가 2개
+pca.fit(data)
+
+pca.components_
+pca.explained_variance_ratio_
+
+predict = pca.transform(data)
+pre2 = data @ pca.components_.T
+
+
+plt.scatter(predict[:,0], predict[:,1])
+plt.show()
+
